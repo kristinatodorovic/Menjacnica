7232d9bc2112697939caba51ba1a9655a8b62775
kristinatodorovic <kristinatodorovic108@gmail.com>
2017-04-03 10:42:48 +0200

    Izmenjena metoda pronadjiKurs u klasi Interfejs.

--- Menjacnica/src/interfejsi/Interfejs.java
+++ Menjacnica/src/interfejsi/Interfejs.java
@@ -2,8 +2,10 @@ package interfejsi;
 
 import java.util.GregorianCalendar;
 
+import kursevi.Kurs;
+
 public interface Interfejs {
 	public void dodajKurs(GregorianCalendar datum, int prodajni, int srednji, int kupovni, String valuta);
 	public void obrisiKurs(GregorianCalendar datum);
-	public void pronadjiKurs(GregorianCalendar datum, String valuta);
+	public Kurs pronadjiKurs(GregorianCalendar datum, String valuta);
 }
