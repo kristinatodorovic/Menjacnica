5c50137b331ea5d693d5f5b2bc7f360ac4953f76
kristinatodorovic <kristinatodorovic108@gmail.com>
2017-04-03 11:03:32 +0200

    Dodata klasa 'Implementacija' koja implementira interfejs.

---
+++ Menjacnica/src/Implementacija.java
@@ -0,0 +1,26 @@
+import java.util.GregorianCalendar;
+
+import interfejsi.Interfejs;
+import kursevi.Kurs;
+
+public class Implementacija implements Interfejs {
+
+	@Override
+	public void dodajKurs(GregorianCalendar datum, int prodajni, int srednji, int kupovni, String valuta) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void obrisiKurs(GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Kurs pronadjiKurs(GregorianCalendar datum, String valuta) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
