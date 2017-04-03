fab33f05bc510f2a0d84ec78dcb98b4ff3ba6835
kristinatodorovic <kristinatodorovic108@gmail.com>
2017-04-03 12:00:17 +0200

    Napisan kod za Implementaciju na master grani.

--- Menjacnica/src/Implementacija.java
+++ Menjacnica/src/Implementacija.java
@@ -2,25 +2,57 @@ import java.util.GregorianCalendar;
 
 import interfejsi.Interfejs;
 import kursevi.Kurs;
+import valute.ValuteMenjacnice;
 
 public class Implementacija implements Interfejs {
-
+	ValuteMenjacnice menjacnica=new ValuteMenjacnice();
 	@Override
 	public void dodajKurs(GregorianCalendar datum, int prodajni, int srednji, int kupovni, String valuta) {
-		// TODO Auto-generated method stub
+		Kurs ks=new Kurs();
+		ks.setDatum(datum);
+		ks.setKupovni(kupovni);
+		ks.setProdajni(prodajni);
+		ks.setSrednji(srednji);
+		for(int i=0;i<menjacnica.getValute().size();i++){
+			if(menjacnica.getValute().get(i).getNazivValute().equals(valuta)){
+				menjacnica.getValute().get(i).getKursevi().add(ks);
+				return;
+			}
+		}
 
 	}
 
 	@Override
-	public void obrisiKurs(GregorianCalendar datum) {
-		// TODO Auto-generated method stub
-
+	public void obrisiKurs(GregorianCalendar datum, String valuta) {
+		
+		for(int i=0;i<menjacnica.getValute().size();i++){
+			if(menjacnica.getValute().get(i).getNazivValute().equals(valuta)){
+				for(int z=0;z<menjacnica.getValute().get(i).getKursevi().size();z++){
+					if(menjacnica.getValute().get(i).getKursevi().get(z).getDatum().equals(datum) && datum!=null){
+						menjacnica.getValute().get(i).getKursevi().remove(z);
+					}
+				}
+				}
+			}
 	}
 
 	@Override
 	public Kurs pronadjiKurs(GregorianCalendar datum, String valuta) {
-		// TODO Auto-generated method stub
-		return null;
+		Kurs ks=new Kurs();
+		ks.setDatum(datum);
+		for(int i=0;i<menjacnica.getValute().size();i++){
+			if(menjacnica.getValute().get(i).getNazivValute().equals(valuta)){
+				for(int z=0;z<menjacnica.getValute().get(i).getKursevi().size();z++){
+					if(menjacnica.getValute().get(i).getKursevi().get(z).getDatum().equals(datum) && datum!=null){
+						ks.setKupovni(menjacnica.getValute().get(i).getKursevi().get(z).getKupovni());
+						ks.setKupovni(menjacnica.getValute().get(i).getKursevi().get(z).getSrednji());
+						ks.setKupovni(menjacnica.getValute().get(i).getKursevi().get(z).getProdajni());
+						
+					}
+				}
+			}
+		}
+		return ks;
 	}
-
+	
 }
--- Menjacnica/src/interfejsi/Interfejs.java
+++ Menjacnica/src/interfejsi/Interfejs.java
@@ -6,6 +6,6 @@ import kursevi.Kurs;
 
 public interface Interfejs {
 	public void dodajKurs(GregorianCalendar datum, int prodajni, int srednji, int kupovni, String valuta);
-	public void obrisiKurs(GregorianCalendar datum);
+	public void obrisiKurs(GregorianCalendar datum, String valuta);
 	public Kurs pronadjiKurs(GregorianCalendar datum, String valuta);
 }
